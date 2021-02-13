76dc2f963ec84e6cf86948e6896843df6aa7b1e8
impypots <72578093+impypots@users.noreply.github.com>
2021-02-13 00:35:18 -0700

    Start

---
+++ personal-site/src/App.js
@@ -0,0 +1,13 @@
+import Header from './header'
+import './App.css';
+import background from './kittyicecream.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-site/src/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div id="HeaderBox">
+        <h4>HOME</h4>
+        <h4>WORK</h4>
+        <h4>CONTACT</h4>
+        </div>
+    )
+}
+
+export default Header;

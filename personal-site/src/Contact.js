a126487f615516212804d5b17470c35075b63f1e
impypots <72578093+impypots@users.noreply.github.com>
2021-02-24 23:01:49 -0700

    Made more mobile friendly

--- personal-site/src/Contact.js
+++ personal-site/src/Contact.js
@@ -5,7 +5,15 @@ function Contact(props) {
         <div id="contact" style={{
             display: props.display
         }}>
-            <h1>This is the contact page, BATCH</h1>
+            <h1>Say Hello!</h1>
+            <h5>Feel free to email me below. Alternatively, you are welcome to call or text me at (615) 318-7701.</h5>
+            <form action="MAILTO:impypots@gmail.com" method="post" enctype="text/plain">
+            <label for="name">Name: </label>
+            <input type="text" id="name" name="user"/>
+            <label htmlFor=""></label>
+
+
+            </form>
         </div>
     )
 }

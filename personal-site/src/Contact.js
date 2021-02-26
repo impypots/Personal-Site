95276f0671258b41898ee671efc19948f36b3d73
impypots <72578093+impypots@users.noreply.github.com>
2021-02-25 21:39:41 -0700

    Got email form working

--- personal-site/src/Contact.js
+++ personal-site/src/Contact.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 function Contact(props) {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_qc5ya8e', 'template_i816mz3', e.target, 'user_qlo8F25xtWkwusQy6gyGG')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+          alert("Something happened!")
+    }
+
     return(
         <div id="contact" style={{
             display: props.display
         }}>
             <h1>Say Hello!</h1>
-            <h5>Feel free to email me below. Alternatively, you are welcome to call or text me at (615) 318-7701.</h5>
-            <form action="MAILTO:impypots@gmail.com" method="post" enctype="text/plain">
-            <label for="name">Name: </label>
-            <input type="text" id="name" name="user"/>
-            <label htmlFor=""></label>
+            <h5>Feel free to email me below. Put something else here later lol.</h5>
+            <form onSubmit={sendEmail}>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name="name" /><br/>
+                <label htmlFor="email">Email: </label>
+                <input type="text" name="email" /><br/>
+                <label htmlFor="email">Subject: </label>
+                <input type="text" name="subject" /><br/>
+                <label htmlFor="message">Message: </label><br/>
+                <textarea name="message" id="" cols="30" rows="10" ></textarea><br/>
+                <input type="submit" value="send email"/>
 
 
             </form>
